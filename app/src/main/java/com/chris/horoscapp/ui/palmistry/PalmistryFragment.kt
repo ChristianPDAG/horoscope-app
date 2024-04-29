a99ae606ef0b414e1ec8f55a2284db7c50b3a386
ChristianPDAG <christian.perezdearce@gmail.com>
2024-04-29 18:07:30 -0400

    initial commit

---
+++ app/src/main/java/com/chris/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -0,0 +1,24 @@
+package com.chris.horoscapp.ui.palmistry
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.chris.horoscapp.R
+import com.chris.horoscapp.databinding.FragmentPalmistryBinding
+
+class PalmistryFragment : Fragment() {
+    private var _binding:FragmentPalmistryBinding? = null
+    private val binding get()= _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
